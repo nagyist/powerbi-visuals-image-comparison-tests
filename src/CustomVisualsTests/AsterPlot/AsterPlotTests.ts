e6521da2069045a70816476b84df1a4c2d45e39a
Denis Kudelin <v-dekude@microsoft.com>
2016-09-21 18:10:37 +0300

    Added assertAreaScreenshot function

--- src/CustomVisualsTests/AsterPlot/AsterPlotTests.ts
+++ src/CustomVisualsTests/AsterPlot/AsterPlotTests.ts
@@ -2,13 +2,11 @@
 
 describe("AsterPlot", () => {
     it("default", (done) => {
-        browser
-            //To execute any client script before taking a screenshot
-            .webdrivercss(testRunner.getCurrentSpecImagePath(), {
+        //To execute any client script before taking a screenshot
+        browser.assertAreaScreenshot({
                 name: "svg.asterPlot",
                 elem: "svg.asterPlot"
             })
-            .then(result => jasmineHelpers.assertImageMatch(result))
             .then(() => done());
     });
 });
